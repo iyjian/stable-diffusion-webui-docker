f8706c5e0332886838b1d577ccf7ffd9fd21888c
Rens <rens23333@gmail.com>
2024-10-14 15:31:06 +0800

    feat: switch image tag to test

--- services/AUTOMATIC1111/init.sh
+++ services/AUTOMATIC1111/init.sh
@@ -1,27 +1,11 @@
 #!/bin/bash
-
-MOUNTS["/root/.cache"]="/data/.cache"
-MOUNTS["${ROOT}/models"]="/data/models"
-
-MOUNTS["${ROOT}/embeddings"]="/data/embeddings"
-MOUNTS["${ROOT}/config.json"]="/data/config/auto/config.json"
-MOUNTS["${ROOT}/ui-config.json"]="/data/config/auto/ui-config.json"
-MOUNTS["${ROOT}/styles.csv"]="/data/config/auto/styles.csv"
-MOUNTS["${ROOT}/extensions"]="/data/config/auto/extensions"
-MOUNTS["${ROOT}/config_states"]="/data/config/auto/config_states"
-
-echo "Installing extension dependencies (if any)"
-
-# because we build our container as root:
-chown -R root ~/.cache/
-chmod 766 ~/.cache/
 cd /data/config/auto/extensions
 git clone https://github.com/Mikubill/sd-webui-controlnet.git
 git clone https://github.com/w-e-w/sd-webui-nudenet-nsfw-censor.git
 git clone https://github.com/iyjian/stable-diffusion-webui-localization-zh_CN.git
 shopt -s nullglob
 # For install.py, please refer to https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Developing-extensions#installpy
-list=(./extensions/*/install.py)
+list=(./*/install.py)
 for installscript in "${list[@]}"; do
   EXTNAME=$(echo $installscript | cut -d '/' -f 3)
   # Skip installing dependencies if extension is disabled in config
