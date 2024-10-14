771604baa2b55b051a2239b35fd0d4eb183a742c
Rens <rens23333@gmail.com>
2024-10-14 15:03:23 +0800

    feat: try add init

--- services/AUTOMATIC1111/entrypoint.sh
+++ services/AUTOMATIC1111/entrypoint.sh
@@ -64,22 +64,22 @@ chmod 766 ~/.cache/
 
 shopt -s nullglob
 # For install.py, please refer to https://github.com/AUTOMATIC1111/stable-diffusion-webui/wiki/Developing-extensions#installpy
-list=(./extensions/*/install.py)
-for installscript in "${list[@]}"; do
-  EXTNAME=$(echo $installscript | cut -d '/' -f 3)
-  # Skip installing dependencies if extension is disabled in config
-  if $(jq -e ".disabled_extensions|any(. == \"$EXTNAME\")" config.json); then
-    echo "Skipping disabled extension ($EXTNAME)"
-    continue
-  fi
-  PYTHONPATH=${ROOT} python "$installscript"
-done
+# list=(./extensions/*/install.py)
+# for installscript in "${list[@]}"; do
+#   EXTNAME=$(echo $installscript | cut -d '/' -f 3)
+#   # Skip installing dependencies if extension is disabled in config
+#   if $(jq -e ".disabled_extensions|any(. == \"$EXTNAME\")" config.json); then
+#     echo "Skipping disabled extension ($EXTNAME)"
+#     continue
+#   fi
+#   PYTHONPATH=${ROOT} python "$installscript"
+# done
 
-if [ -f "/data/config/auto/startup.sh" ]; then
-  pushd ${ROOT}
-  echo "Running startup script"
-  . /data/config/auto/startup.sh
-  popd
-fi
+# if [ -f "/data/config/auto/startup.sh" ]; then
+#   pushd ${ROOT}
+#   echo "Running startup script"
+#   . /data/config/auto/startup.sh
+#   popd
+# fi
 
 exec "$@"
