ddc66a129fcce4d07f96a8e8f437cf16ae5f9c20
Rens <rens23333@gmail.com>
2024-10-14 21:05:44 +0800

    fix: controlnrt

--- services/AUTOMATIC1111/init.sh
+++ services/AUTOMATIC1111/init.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 mkdir -p /data/config/auto/extensions
 cd /data/config/auto/extensions
-git clone https://github.com/Mikubill/sd-webui-controlnet.git /sd-webui-controlnet
-cd /sd-webui-controlnet
+git clone https://github.com/Mikubill/sd-webui-controlnet.git
+cd sd-webui-controlnet
 git checkout a5457dcc1c952a400b84a2ab90eade47c0d7fed7
 cd /data/config/auto/extensions
 git clone https://github.com/w-e-w/sd-webui-nudenet-nsfw-censor.git
